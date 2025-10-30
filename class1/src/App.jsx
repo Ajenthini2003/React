1b46a7e5c06b22a6e04cb94c24d03f91df45c8b2
Ajenthini2003 <ajenthini.uki.stu@dreamspace.academy>
2025-10-30 08:23:52 +0530

    created files

--- class1/src/App.jsx
+++ class1/src/App.jsx
@@ -4,8 +4,14 @@
 
 //import { Header } from "./components/Header"
 //import { Hooks01 } from "./components/Hooks01"
-import { ToDolist } from "./components/ToDolist"
-
+// import { ToDolist } from "./components/ToDolist"
+// import Calculate from './components/day6/Calculate';
+// import Hooks from './components/day6/Hooks';
+// import ButtonLike from './components/day7/ButtonClick';
+// import Like from './components/day7/Like'
+// import Form1 from './components/day9/Form1';
+// import Task from './components/day8(forms)/Task';
+import Form2 from './components/Classwork/Form2'
 function App() {
   
 
@@ -18,7 +24,8 @@ function App() {
      <Student name="sanojan" age={22} isMarried={true}></Student>
    <Student name="vihya" ></Student> */}
    {/* <Hooks01></Hooks01> */}
-   <ToDolist></ToDolist>
+   {/* <ToDolist></ToDolist> */}
+   <Form2></Form2>
    </div>
   )
 }
